c7d55c342d295d16a83d324df29dceb6390324cf
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2026-02-24 22:38:18 +0530

    Fix insertion sort loop initialization

--- Algorithms/InsertionSort.java
+++ Algorithms/InsertionSort.java
@@ -11,7 +11,7 @@ public class InsertionSort {
     }
     public static void insertionSort(int[] arr){
         int len = arr.length;
-        for(int i = 0; i < len; i++){
+        for(int i = 1; i < len; i++){
             int key = arr[i];
             int j = i - 1;
             while(j >= 0 && arr[j] > key){
